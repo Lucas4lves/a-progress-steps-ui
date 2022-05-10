0e2aba5d6a14b8254bb2bef75efacbefc49a0b2f
Lucas Alves <zombnfl@gmail.com>
2022-05-10 20:18:51 -0300

    Worked on how to increment the progress bar each step

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const progressBar = document.querySelector('.progress');
-
-console.log(progressBar);
+let multi = 1;
 
 function trackProgress(){
-    progressBar.classList.add('progress25');
+    progressBar.classList.add('dynamicProgress');
 
 }
-
+function incProgress(){
+    if(multi === 9){
+        return
+    }
+    const styles = [`
+    .dynamicProgress {
+        width: calc(42.5px*${multi});
+        transition: width 1s ease-out;
+    }
+    `]
+    trackProgress();
+    const styleSheet = document.createElement('style');
+    styleSheet.innerText = styles;
+    document.head.appendChild(styleSheet);
+    multi += 2;
+    
+}
